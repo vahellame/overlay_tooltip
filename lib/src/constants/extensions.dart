6a03b131d8172b9af3072f0e2e3f7f4aa93c7126
Vadim R <vahellame@gmail.com>
2023-06-16 17:39:33 +0300

    Pre-final

--- lib/src/constants/extensions.dart
+++ lib/src/constants/extensions.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 extension GlobalKeyEx on GlobalKey {
   Rect? get globalPaintBounds {
-    final renderObject = currentContext?.findRenderObject();
-    if (renderObject?.attached ?? false) {
-      var translation = renderObject?.getTransformTo(null).getTranslation();
-      if (translation != null && renderObject?.paintBounds != null) {
-        return renderObject!.paintBounds
-            .shift(Offset(translation.x, translation.y));
+    try {
+      final renderObject = currentContext?.findRenderObject();
+      if (renderObject?.attached ?? false) {
+        var translation = renderObject?.getTransformTo(null).getTranslation();
+        if (translation != null && renderObject?.paintBounds != null) {
+          return renderObject!.paintBounds.shift(Offset(translation.x, translation.y));
+        }
       }
-    }
+    } catch (_) {}
+
     return null;
   }
 }
